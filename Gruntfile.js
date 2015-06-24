543c567e7059120256980b3f3e9c500d347951aa
Oleg Boroday <oleg.boroday@softcube.com>
2015-06-24 23:31:59 +0200

    feat: add html minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,20 +4,38 @@ module.exports = function(grunt) {
         copy: {
             build: {
                 src: ['**', '!build/**', '!node_modules/**',
-                      '!Gruntfile.js', '!ngrok', '!package.json',
-                      '!README.md', '!TimelineBeforeOptimizations',
-                      '!**/*.css', '!**/*.js'],
+                    '!Gruntfile.js', '!ngrok', '!package.json',
+                    '!README.md', '!TimelineBeforeOptimizations',
+                    '!**/*.css', '!**/*.js', '!**/*.html'
+                ],
                 dest: 'build',
                 expand: true
+            }
+        },
+        clean: ['build'],
+        htmlmin: {
+            html: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    minifyJS: true,
+                    minifyCSS: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '.',
+                    src: ['**/*.html', '!node_modules/**', '!build/**'],
+                    dest: 'build/'
+                }]
             }
         },
         cssmin: {
             css: {
                 files: [{
+                    expand: true,
                     cwd: '.',
-                    src: ['**/*.css', '!node_modules/**'],
-                    dest:'build/',
-                    expand: true
+                    src: ['**/*.css', '!node_modules/**', '!build/**'],
+                    dest: 'build/'
                 }]
             }
         },
@@ -25,7 +43,8 @@ module.exports = function(grunt) {
             js: {
                 files: [{
                     cwd: '.',
-                    src: ['**/*.js', '!node_modules/**'],
+                    src: ['**/*.js', '!node_modules/**', '!build/**',
+                          '!Gruntfile.js'],
                     dest: 'build/',
                     expand: true
                 }]
@@ -37,5 +56,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.registerTask('default', ['copy', 'cssmin:css', 'uglify:js']);
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('default', ['clean', 'copy', 'htmlmin:html', 'cssmin:css', 'uglify:js']);
 };
