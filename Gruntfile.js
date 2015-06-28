4a2c87e28d40213e8de38e69a1bfb9b3f07230b9
Oleg Boroday <oleg.boroday@softcube.com>
2015-06-28 12:07:10 +0200

    feat: optimizations done

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,9 @@ module.exports = function(grunt) {
                 src: ['**', '!build/**', '!node_modules/**',
                     '!Gruntfile.js', '!ngrok', '!package.json',
                     '!README.md', '!TimelineBeforeOptimizations',
-                    '!**/*.css', '!**/*.js', '!**/*.html'
+                    '!**/*.css', '!**/*.js', '!**/*.html',
+                    '!views/images/pizzeria.jpg',
+                    '!img/profilepic.jpg'
                 ],
                 dest: 'build',
                 expand: true
@@ -44,10 +46,68 @@ module.exports = function(grunt) {
                 files: [{
                     cwd: '.',
                     src: ['**/*.js', '!node_modules/**', '!build/**',
-                          '!Gruntfile.js'],
+                        '!Gruntfile.js'
+                    ],
                     dest: 'build/',
                     expand: true
                 }]
+            }
+        },
+        responsive_images: {
+            pizzeria: {
+                options: {
+                    engine: 'im',
+                    sizes: [{
+                        rename: false,
+                        width: 100,
+                        quality: 90
+                    }]
+                },
+                files: {
+
+                    'build/views/images/pizzeria.jpg': 'views/images/pizzeria.jpg',
+
+                }
+            },
+            logo: {
+                options: {
+                    engine: 'im',
+                    sizes: [{
+                        rename: false,
+                        width: 70,
+                        quality: 90
+                    }]
+                },
+                files: {
+
+                    'build/img/profilepic.jpg': 'img/profilepic.jpg',
+
+                }
+            }
+        },
+        imagemin: {
+            images: {
+                options: { // Target options
+                    optimizationLevel: 3,
+                    progressive: false,
+                    interlaced: false
+                },
+                files: { // Dictionary of files
+                    'build/views/images/pizzeria.jpg': 'build/views/images/pizzeria.jpg',
+                    'build/img/profilepic.jpg': 'build/img/profilepic.jpg'
+                }
+            }
+        },
+        compress: {
+            main: {
+                options: {
+                    mode: 'gzip'
+                },
+                expand: true,
+                cwd: 'build/',
+                src: ['**/*',
+                ],
+                dest: 'build/'
             }
         }
     });
@@ -58,5 +118,16 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.registerTask('default', ['clean', 'copy', 'htmlmin:html', 'cssmin:css', 'uglify:js']);
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.registerTask('default', ['clean',
+        'copy',
+        'htmlmin:html',
+        'cssmin:css',
+        'uglify:js',
+        'responsive_images',
+        'imagemin'//,
+        //'compress'
+    ]);
 };
